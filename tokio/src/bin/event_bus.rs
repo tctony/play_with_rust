0440d1b9c26b0d38d855903df1e78e7a4d25d7eb
Tony Tang <tangchang21@gmail.com>
2024-10-11 11:41:50 +0800

    add test case for event bus

--- tokio/src/bin/event_bus.rs
+++ tokio/src/bin/event_bus.rs
@@ -2,8 +2,9 @@ use futures::{stream::BoxStream, StreamExt};
 use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc::UnboundedSender;
 
+#[derive(Debug, Clone)]
 pub struct EventBus<T> {
-    subscribers: Mutex<Vec<UnboundedSender<Arc<T>>>>,
+    subscribers: Arc<Mutex<Vec<UnboundedSender<Arc<T>>>>>,
 }
 
 impl<T: 'static + Send + Sync> EventBus<T> {
@@ -32,7 +33,7 @@ impl<T: 'static + Send + Sync> EventBus<T> {
     }
 }
 
-pub trait Subscribable<T: 'static + Clone + Send + Sync> {
+pub trait EventBusTrait<T: 'static + Clone + Send + Sync> {
     fn event_bus(&self) -> &EventBus<T>;
 
     fn subscribe_change(&self) -> BoxStream<Arc<T>> {
@@ -48,10 +49,77 @@ pub trait Subscribable<T: 'static + Clone + Send + Sync> {
 mod tests {
     use super::*;
 
-    // WIP
     #[tokio::test]
     async fn test_event_bus() {
-        // WIP
+        let bus = EventBus::<i32>::new();
+
+        let handle = tokio::spawn({
+            let bus = bus.clone();
+            async move {
+                for i in 0..5 {
+                    println!("emit {}", i);
+                    bus.emit(i);
+
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+            }
+        });
+
+        tokio::time::sleep(tokio::time::Duration::from_millis(1_500)).await;
+        tokio::spawn({
+            let bus = bus.clone();
+            async move {
+                let mut stream = bus.subscribe();
+                while let Some(event) = stream.next().await {
+                    println!("recv {:?}", event);
+                }
+            }
+        });
+
+        let _ = handle.await;
+    }
+
+    #[derive(Debug, Clone)]
+    struct EventBusWrapper {
+        pub internal: EventBus<i32>,
+    }
+
+    impl EventBusTrait<i32> for EventBusWrapper {
+        fn event_bus(&self) -> &EventBus<i32> {
+            &self.internal
+        }
+    }
+
+    #[tokio::test]
+    async fn test_event_bus_trait() {
+        let bus = EventBusWrapper {
+            internal: EventBus::<i32>::new(),
+        };
+
+        let handle = tokio::spawn({
+            let bus = bus.clone();
+            async move {
+                for i in 0..5 {
+                    println!("emit {}", i);
+                    bus.internal.emit(i);
+
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+            }
+        });
+
+        tokio::time::sleep(tokio::time::Duration::from_millis(1_500)).await;
+        tokio::spawn({
+            let bus = bus.clone();
+            async move {
+                let mut stream = bus.subscribe_change();
+                while let Some(event) = stream.next().await {
+                    println!("recv {:?}", event);
+                }
+            }
+        });
+
+        let _ = handle.await;
     }
 }
 
